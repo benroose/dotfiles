ddf70a7fcf71ce5cae940da814ec541c9e0173ce
Ben Roose <ben.roose@wichita.edu>
2019-06-10 11:08:18 -0700

    Add xrandr screenlayout scripts

---
+++ .screenlayout/vga-mirrored.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS-1 --primary --mode 1280x800 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --off --output HDMI-1 --off --output VGA-1 --mode 640x480 --pos 640x80 --rotate normal
